4d6aaa8d9618287638776d526cf048b3b12540b9
Mateusz Kurek <m.kurek1994@gmail.com>
2023-07-07 00:24:45 +0200

    add sizes section

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/components/size-form.tsx
@@ -0,0 +1,174 @@
+"use client";
+
+import { useState } from "react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Size } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+import { AlertModal } from "@/components/modals/alert-modal";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  value: z.string().min(1),
+});
+
+type SizeFormValues = z.infer<typeof formSchema>;
+
+interface SizeFormProps {
+  initialData: Size | null;
+}
+
+export const SizeForm: React.FC<SizeFormProps> = ({
+  initialData,
+}) => {
+  const params = useParams();
+  const router = useRouter();
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const title = initialData ? "Edit size" : "Create size";
+  const description = initialData ? "Edit a size" : "Add a new size";
+  const toastMessage = initialData
+    ? "Size updated."
+    : "Size created.";
+  const actionLabel = initialData ? "Save changes" : "Create";
+
+  const form = useForm<SizeFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: "",
+      value: "",
+    },
+  });
+
+  const onSubmit = async (values: SizeFormValues) => {
+    setIsLoading(true);
+
+    try {
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/sizes/${params.sizeId}`,
+          values
+        );
+      } else {
+        await axios.post(`/api/${params.storeId}/sizes`, values);
+      }
+      router.refresh();
+      router.push(`/${params.storeId}/sizes`);
+      toast.success(toastMessage);
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    setIsLoading(true);
+    axios
+      .delete(`/api/${params.storeId}/sizes/${params.sizeId}`)
+      .then(() => {
+        router.refresh();
+        router.push(`/${params.storeId}/sizes`);
+        toast.success("Size deleted");
+      })
+      .catch(() => {
+        toast.error("Make sure you removed all categories using this billboard");
+      })
+      .finally(() => {
+        setIsLoading(false);
+        setIsOpen(false);
+      });
+  };
+
+  return (
+    <>
+      <AlertModal
+        isLoading={isLoading}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        onConfirm={onDelete}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+        {initialData ? (
+          <Button
+            disabled={isLoading}
+            variant="destructive"
+            size="sm"
+            onClick={() => {
+              setIsOpen(true);
+            }}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        ) : null}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              name="name"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder="Size name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="value"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Value</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder="Size value"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={isLoading} type="submit">
+            {actionLabel}
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
---
+++ app/api/[storeId]/sizes/route.ts
@@ -0,0 +1,75 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!body.name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!body.value) {
+      return new NextResponse("Value is required", { status: 400 });
+    }
+
+    if(!params.storeId) {
+        return new NextResponse("Store Id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId,
+            userId: userId,
+        }
+    })
+
+    if(!storeByUserId) {
+        return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const size = await prismadb.size.create({
+      data: {
+        name: body.label,
+        value: body.value,
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(size);
+  } catch (error) {
+    console.log("[SIZES_POST]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function GET(
+    _req: Request,
+    { params }: { params: { storeId: string } }
+  ) {
+    try {
+      if(!params.storeId) {
+          return new NextResponse("Store Id is required", { status: 400 });
+      }
+    
+      const size = await prismadb.size.findMany({
+        where: {
+            storeId: params.storeId,
+        }
+      });
+  
+      return NextResponse.json(size);
+    } catch (error) {
+      console.log("[SIZES_GET]", error);
+      return new NextResponse("Internal error", { status: 500 });
+    }
+  }
