57e0b25c9507531e6163059a86b94c00ee94c725
Mateusz Kurek <m.kurek1994@gmail.com>
2023-07-10 16:18:29 +0200

    Add colors section

---
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/components/color-form.tsx
@@ -0,0 +1,179 @@
+"use client";
+
+import { useState } from "react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Color } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+import { AlertModal } from "@/components/modals/alert-modal";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  value: z.string().min(4).regex(/^#/, {
+    message: 'String must be a valid hex code',
+  }),
+});
+
+type ColorFormValues = z.infer<typeof formSchema>;
+
+interface ColorFormProps {
+  initialData: Color | null;
+}
+
+export const ColorForm: React.FC<ColorFormProps> = ({
+  initialData,
+}) => {
+  const params = useParams();
+  const router = useRouter();
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const title = initialData ? "Edit color" : "Create color";
+  const description = initialData ? "Edit a color" : "Add a new color";
+  const toastMessage = initialData
+    ? "Color updated."
+    : "Color created.";
+  const actionLabel = initialData ? "Save changes" : "Create";
+
+  const form = useForm<ColorFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: "",
+      value: "",
+    },
+  });
+
+  const onSubmit = async (values: ColorFormValues) => {
+    setIsLoading(true);
+
+    try {
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/colors/${params.colorId}`,
+          values
+        );
+      } else {
+        await axios.post(`/api/${params.storeId}/colors`, values);
+      }
+      router.refresh();
+      router.push(`/${params.storeId}/colors`);
+      toast.success(toastMessage);
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    setIsLoading(true);
+    axios
+      .delete(`/api/${params.storeId}/colors/${params.colorId}`)
+      .then(() => {
+        router.refresh();
+        router.push(`/${params.storeId}/colors`);
+        toast.success("Color deleted");
+      })
+      .catch(() => {
+        toast.error("Make sure you removed all categories using this billboard");
+      })
+      .finally(() => {
+        setIsLoading(false);
+        setIsOpen(false);
+      });
+  };
+
+  return (
+    <>
+      <AlertModal
+        isLoading={isLoading}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        onConfirm={onDelete}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+        {initialData ? (
+          <Button
+            disabled={isLoading}
+            variant="destructive"
+            size="sm"
+            onClick={() => {
+              setIsOpen(true);
+            }}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        ) : null}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              name="name"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder="Color name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="value"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Value</FormLabel>
+                  <FormControl>
+                    <div className="flex items-center gap-x-4">
+                    <Input
+                      disabled={isLoading}
+                      placeholder="Color value"
+                      {...field}
+                    />
+                    <div className="border p-4 rounded-full" style={{backgroundColor: field.value}}/>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={isLoading} type="submit">
+            {actionLabel}
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
