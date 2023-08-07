0c195e59c2e89920c7246fbe9742c017abe4e296
Mateusz Kurek <m.kurek1994@gmail.com>
2023-08-07 20:05:24 +0200

    add dark mode toggle button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import { MainNav } from "@/components/main-nav";
 import { StoreSwitcher } from "@/components/store-switcher";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import { ModeToggle } from "./theme-toggle";
 
 export const Navbar = async () => {
   const { userId } = auth();
@@ -24,6 +25,7 @@ export const Navbar = async () => {
         <StoreSwitcher items={stores}/>
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
+          <ModeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
